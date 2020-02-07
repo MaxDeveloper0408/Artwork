fb2fe17387986ea1f175a5eb37adaa33ecf02171
webdevsmart <webdevsmart@hotmail.com>
2020-02-07 22:53:01 +0300

    refine code

--- accounts/mailers.py
+++ accounts/mailers.py
@@ -6,27 +6,30 @@ from email.mime.multipart import MIMEMultipart
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
+
 def TEXT(token):
     return f'{HOST}/api/accounts/activate/{token}'
+
 
 def HTML(token):
     return f'<h5><a href="{HOST}/api/accounts/activate/{token}">Click here to activate account</a></h5>'
 
+
 def ResetTEXT(token):
     return f'{HOST}/reset-password/{token}'
+
 
 def ResetHTML(token):
     return f'<h5><a href="{HOST}/reset-password/{token}">Click here to reset yourpassword</a></h5>'
 
-def SendEmail(to,subject,token,activation=True):
 
+def SendEmail(to, subject, token, activation=True):
     if activation:
         text = TEXT(token)
         html = HTML(token)
     else:
         text = ResetTEXT(token)
         html = ResetHTML(token)
-
 
     from_email = EMAIL_FROM
     msg = EmailMultiAlternatives(subject, text, from_email, [to])
@@ -43,8 +46,7 @@ def SendEmail(to,subject,token,activation=True):
     #           "html": html})
 
 
-def send_buy_link(to,subject,link):
-
+def send_buy_link(to, subject, link):
     text = f'{link}'
     html = f'<a href="{link}">Buy</a>'
 
