d4d4b6b7e2e1c8ea31aa9c849356c38b8ccdb84a
seday72 <seday72@yandex.com>
2020-02-07 18:50:27 +1000

    add alpha version of Google Account Login logic

--- accounts/views.py
+++ accounts/views.py
@@ -6,30 +6,32 @@ from rest_framework.decorators import action
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from .forms import SignupForm,AuthenticationForm
+from .forms import SignupForm, AuthenticationForm
 from django.contrib.auth.forms import SetPasswordForm
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.exceptions import ValidationError
 from payments.models import PaymentMethod
+from google.oauth2 import id_token
+from google.auth.transport import requests
+
 
 class Signup(viewsets.ViewSet):
-
     permission_classes = (AllowAny,)
-    http_method_names = ['post',]
+    http_method_names = ['post', ]
 
     def create(self, request, *args, **kwargs):
 
         form_data = {
-                'username':request.data.get('username').lower(),
-                'password1':request.data.get('password'),
-                'password2':request.data.get('confirm_password'),
-                'email':request.data.get('email'),
-                'role':request.data.get('role'),
-                }
+            'username': request.data.get('username').lower(),
+            'password1': request.data.get('password'),
+            'password2': request.data.get('confirm_password'),
+            'email': request.data.get('email'),
+            'role': request.data.get('role'),
+        }
 
-        valid_roles = ['A','C']
+        valid_roles = ['A', 'C']
 
         form = SignupForm(form_data)
         messages = form.errors.get_json_data()
@@ -40,21 +42,20 @@ class Signup(viewsets.ViewSet):
                 activation_secret = auth.makesecret(form_data['username'])
 
                 Profile.objects.filter(user_id=obj.id).update(role=form_data['role'],
-                                           activation_secret=activation_secret)
+                                                              activation_secret=activation_secret)
 
-
-                token = auth.makeJWT(form_data['username'],activation_secret)
+                token = auth.makeJWT(form_data['username'], activation_secret)
                 method = PaymentMethod.objects.filter(user_id=obj.id).first()
                 if method:
                     is_stripe_connected = True
                 else:
                     is_stripe_connected = False
 
-                SendEmail(form_data['email'],EMAIL_ACTIVATION_SUB,token)
+                SendEmail(form_data['email'], EMAIL_ACTIVATION_SUB, token)
                 token, created = Token.objects.get_or_create(user=obj)
                 data = {'status': True, 'token': token.key,
-                        "verified":obj.profile.is_verified,"is_profile_complete":False,
-                        'is_stripe_connected':is_stripe_connected}
+                        "verified": obj.profile.is_verified, "is_profile_complete": False,
+                        'is_stripe_connected': is_stripe_connected}
 
                 return Response(data)
             else:
@@ -66,7 +67,7 @@ class Signup(viewsets.ViewSet):
                 if messages.get('password1'):
                     messages.pop('password1')
 
-        return Response({"status":False,"errors":messages},status=400)
+        return Response({"status": False, "errors": messages}, status=400)
 
 
 class Login(viewsets.ViewSet):
@@ -77,42 +78,60 @@ class Login(viewsets.ViewSet):
         email = request.data.get('email').lower()
         print(email)
 
+        # verify google idToken in the case of social login
+        is_social_login = request.data.get('isSocial')
+        if is_social_login:
+            google_request = requests.Request()
+            token = request.data.get('idToken')
+            try:
+                id_info = id_token.verify_oauth2_token(token,
+                                                       google_request,
+                                                       '465669180733-qn10t5c2ebud38t9lmfvcrifsbokhs0v.apps.googleusercontent.com')
+            except ValueError:
+                result = {'status': 'error', 'message': 'Invalid social user', 'code': -1001}
+                return Response(result, status=401)
+
+            if id_info['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+                result = {'status': 'error', 'message': 'Invalid social network issuer', 'code': -1001}
+                return Response(result, status=401)
+
+        # check if the user exists
         try:
             user = User.objects.get(email=email)
-            form_data = {'username':user.username,'password':request.data.get('password')}
-
+            form_data = {'username': user.username, 'password': request.data.get('password')}
         except User.DoesNotExist:
+            # if login is not via social, return error
+            if is_social_login is False:
+                result = {'status': 'error', 'message': 'User does not exist', 'code': -1002}
+                return Response(result, status=401)
+            # redirect signup page
+            result = {'status': 'error', 'message': 'Social user should complete their profile', 'code': -1003}
+            return Response(result)
 
-            err = {"errors": [{"message":"User does not exist", "code": "und"}]}
-            return Response(err,status=400)
-
-        form  = AuthenticationForm(data=form_data)
-
-        if form.is_valid():
-            user = form.get_user()
-            token, created = Token.objects.get_or_create(user=user)
-
-            is_profile_complete = False
-            if user.first_name and user.last_name and user.profile.primary_address:
-                is_profile_complete  = True
-
-            method = PaymentMethod.objects.filter(user=user).first()
-            if method and method.is_active():
-                is_stripe_connected = True
+        # check password
+        if is_social_login is False:
+            form = AuthenticationForm(data=form_data)
+            if form.is_valid():
+                user = form.get_user()
             else:
-                is_stripe_connected = False
+                result = {'status': 'error', 'message': 'Invalid email or password', 'code': -1004}
+                return Response(result, status=401)
 
-            data = {'token': token.key,"verified":user.profile.is_verified,
-                    "is_profile_complete":is_profile_complete,'is_stripe_connected':is_stripe_connected}
-            return Response(data)
+        token, created = Token.objects.get_or_create(user=user)
+        is_profile_complete = False
+        if user.first_name and user.last_name and user.profile.primary_address:
+            is_profile_complete = True
 
+        method = PaymentMethod.objects.filter(user=user).first()
+        if method and method.is_active():
+            is_stripe_connected = True
         else:
+            is_stripe_connected = False
 
-            err = {"errors":[]}
-            for error in form.errors.get_json_data().values():
-                err['errors'].extend(error)
-
-        return Response(err,status=400)
+        result = {'status': 'success', 'data': {'token': token.key, 'verified': user.profile.is_verified,
+                                                'is_profile_complete': is_profile_complete,
+                                                'is_stripe_connected': is_stripe_connected}}
+        return Response(result)
 
 
 class ForgotPassword(viewsets.ViewSet):
@@ -142,21 +161,19 @@ class ForgotPassword(viewsets.ViewSet):
 
         if user:
             secret = user.profile.update_secret
-            token = auth.makeJWT(user.username,secret)
-            SendEmail(user.email, 'Forgot Password', token,activation=False)
-            return Response({'message':'Please check your email.'})
+            token = auth.makeJWT(user.username, secret)
+            SendEmail(user.email, 'Forgot Password', token, activation=False)
+            return Response({'message': 'Please check your email.'})
 
-
-        return Response({'error':error},status=401)
+        return Response({'error': error}, status=401)
 
 
 class ResetPassword(APIView):
     permission_classes = (AllowAny,)
 
-
     def get(self, request, *args, **kwargs):
 
-        secret  = kwargs.get('secret')
+        secret = kwargs.get('secret')
         data = auth.checkJWT(secret)
         if data:
             username = data['username']
@@ -164,11 +181,11 @@ class ResetPassword(APIView):
             try:
                 user = User.objects.get(username=username)
                 if user.profile.activation_secret == key:
-                    return Response({'valid':True})
+                    return Response({'valid': True})
             except:
                 pass
 
-        return Response({'valid': False},status=400)
+        return Response({'valid': False}, status=400)
 
     def post(self, request, *args, **kwargs):
 
@@ -204,26 +221,25 @@ class ResetPassword(APIView):
                     message['confirm_password'] = message.pop('new_password2')
 
                 message['valid'] = True
-                return JsonResponse(message,status=401)
+                return JsonResponse(message, status=401)
 
         return JsonResponse({'valid': False}, status=401)
 
 
 class ProfileViewSet(viewsets.ViewSet):
-
-    http_method_names = ['get','put']
+    http_method_names = ['get', 'put']
 
     def list(self, request):
         queryset = Profile.objects.get(user=request.user)
         serializer = ProfileSerializer(queryset)
         return Response(serializer.data)
 
-    @action(['PUT'],False)
+    @action(['PUT'], False)
     def update_profile(self, request, *args, **kwargs):
 
         mutable_data = request.data
-        username = mutable_data.pop('username',False)
-        password = mutable_data.pop('password',False)
+        username = mutable_data.pop('username', False)
+        password = mutable_data.pop('password', False)
         primary_address = request.data.get('primary_address')
 
         if username:
@@ -231,12 +247,12 @@ class ProfileViewSet(viewsets.ViewSet):
         if password:
             raise ValidationError(detail={"password": ["Use Password api end point to change password."]})
 
-        u_serializer = UserSerializer(request.user,data=mutable_data,partial=True)
+        u_serializer = UserSerializer(request.user, data=mutable_data, partial=True)
         u_serializer.is_valid(raise_exception=True)
         u_serializer.save()
 
         p_instance = Profile.objects.get(user=request.user)
-        p_serializer = ProfileSerializer(p_instance,data=mutable_data,partial=True)
+        p_serializer = ProfileSerializer(p_instance, data=mutable_data, partial=True)
         p_serializer.is_valid(raise_exception=True)
 
         if primary_address:
@@ -257,8 +273,8 @@ class ProfileViewSet(viewsets.ViewSet):
     def change_password(self, request, *args, **kwargs):
         password = request.data.get('password')
         confirm_password = request.data.get('confirm_password')
-        form_data = {"new_password1":password,"new_password2":confirm_password}
-        change_password_form = SetPasswordForm(request.user,form_data)
+        form_data = {"new_password1": password, "new_password2": confirm_password}
+        change_password_form = SetPasswordForm(request.user, form_data)
         if change_password_form.is_valid():
             change_password_form.save()
             message = {"status": True}
@@ -270,19 +286,19 @@ class ProfileViewSet(viewsets.ViewSet):
                 message['password'] = message.pop('new_password1')
             message['confirm_password'] = message.pop('new_password2')
 
-            return Response(message,status=401)
+            return Response(message, status=401)
 
     @action(['PUT'], False)
-    def updateimage(self,request, *args, **kwargs):
+    def updateimage(self, request, *args, **kwargs):
         image = {'image': request.FILES.get('image')}
         p_instance = Profile.objects.get(user=request.user)
-        serializer  = ProfileImageSerializer(p_instance,data=image,partial=True)
+        serializer = ProfileImageSerializer(p_instance, data=image, partial=True)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data)
 
 
-def activate(request,secret):
+def activate(request, secret):
     """
     Activate user Profile
 
@@ -300,20 +316,20 @@ def activate(request,secret):
 
     """
     # activate user  Profile
-    #check jwt and extract data
+    # check jwt and extract data
     data = auth.checkJWT(secret)
     if data:
-        #if data is available
-        #get usrname from data
+        # if data is available
+        # get usrname from data
         user = User.objects.get(username=data['username'])
-        #get profile object of user
+        # get profile object of user
         AccountObj = Profile.objects.get(user=user)
 
-        #if key matches to token
+        # if key matches to token
         if AccountObj.activation_secret == data['key']:
-            #create another token
-            AccountObj.activation_secret  = auth.makesecret()
-            AccountObj.is_verified  = True
+            # create another token
+            AccountObj.activation_secret = auth.makesecret()
+            AccountObj.is_verified = True
             user.save()
             AccountObj.save()
 
@@ -322,11 +338,3 @@ def activate(request,secret):
         return render(request, 'accounts/invalid_signup.html')
     else:
         return render(request, 'accounts/invalid_signup.html')
-
-
-
-
-
-
-
-
